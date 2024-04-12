c1460ba00370f6378da48e558f451cd18ee49e0c
jahzserrano <serranojahaziel2@gmail.com>
2024-04-12 08:07:59 -0400

    pydocstyle standards

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" starts a flask web app """
+"""Starts a flask web app."""
 from models import storage
 from flask import Flask
 from flask import render_template
@@ -9,14 +9,14 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """ displays an html page with a list of all states """
+    """Display an html page with a list of all state."""
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """ removes the current session """
+    """Remove the current session """
     storage.close()
 
 
